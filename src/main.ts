93666fbb91402c87ffd620f033e410934d731617
EsdrasALT <esdras.a.pessoal@gmail.com>
2026-05-26 00:08:15 -0300

    semTempoIrmao

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router'; // Linha restaurada
+import { RouteReuseStrategy, provideRouter } from '@angular/router'; // Importação limpa
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
@@ -19,7 +19,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), // Linha restaurada
+    provideRouter(routes),
 
     // --- INICIALIZAÇÃO DO FIREBASE ---
     provideFirebaseApp(() => initializeApp(environment.firebase)),
